a31733605faa388f8952a2fbef7731ef32ca89b8
Sam Mair <sam.j.mair@gmail.com>
2018-04-25 12:30:28 +1000

    updated EntityEvent
    added xml comments to EntityEvent

--- Entity System/EntityEvent.cs
+++ Entity System/EntityEvent.cs
@@ -6,26 +6,49 @@ namespace XenoEngine
 {
     public static class SystemEvents
     {
+        /// <summary>
+        /// Dictionary the stores all events.
+        /// </summary>
         static Dictionary<string, EntityEvent<dynamic>> m_events = new Dictionary<string, EntityEvent<dynamic>>();
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// Static Function Gets the even of a particular name
+        /// </summary>
+        /// <param name="szEventName">name of event</param>
+        /// <returns>returns event</returns>
         //-------------------------------------------------------------------------------
         public static EntityEvent<dynamic> GetEvent(this string szEventName)
         {
             return m_events[szEventName];
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// Static Function creates an event.
+        /// </summary>
+        /// <param name="szEventName">name of event to be created</param>
         //-------------------------------------------------------------------------------
         private static void CreateEvent(string szEventName)
         {
             m_events.Add(szEventName, new EntityEvent<dynamic>());
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// Static Function executes event.
+        /// </summary>
+        /// <param name="szEventName">name of event.</param>
+        /// <param name="sendingEntity">the entity that is sending the event.</param>
+        /// <param name="data">any data that is sent with the event.</param>
         //-------------------------------------------------------------------------------
         public static void FireEvent(this string szEventName, Entity sendingEntity, dynamic data)
         {
             m_events[szEventName].FireEvent(sendingEntity, data);
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// Static Function adds an eventhandler to an existing event or it will create a new event if doesn't exist.
+        /// </summary>
+        /// <param name="szEventName">name of event.</param>
+        /// <param name="handler">the event handler.</param>
         //-------------------------------------------------------------------------------
         public static void AddEventHandler(this string szEventName, Action<Entity, dynamic> handler)
         {
@@ -40,6 +63,11 @@ namespace XenoEngine
             }
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// removes event handler from event.
+        /// </summary>
+        /// <param name="szEventName">name of event.</param>
+        /// <param name="handler">the event handler.</param>
         //-------------------------------------------------------------------------------
         public static void RemoveEventHandler(this string szEventName, Action<Entity, dynamic> handler)
         {
@@ -53,18 +81,30 @@ namespace XenoEngine
             }
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// clears a registered event.
+        /// </summary>
+        /// <param name="szEventName">name of event.</param>
         //-------------------------------------------------------------------------------
         public static void ClearEvent(this string szEventName)
         {
             m_events[szEventName] = null;
         }
-
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// clears the event collection.
+        /// </summary>
+        //-------------------------------------------------------------------------------
         public static void ClearEvents()
         {
             m_events.Clear();
         }
     }
     //-------------------------------------------------------------------------------
+    /// <summary>
+    /// EntityEvent class.
+    /// </summary>
+    /// <typeparam name="TParameter">type of date to be store with event.</typeparam>
     //-------------------------------------------------------------------------------
     [Serializable]
     public class EntityEvent<TParameter>
@@ -74,6 +114,12 @@ namespace XenoEngine
         private object                      m_lockObject = new object();
 
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// operator +. overrides + operator to add handlers to events easily.
+        /// </summary>
+        /// <param name="entityEvent">the event you are join to another event.</param>
+        /// <param name="action">the event handler</param>
+        /// <returns>the new event</returns>
         //-------------------------------------------------------------------------------
         public static EntityEvent<TParameter> operator +(EntityEvent<TParameter> entityEvent, Action<Entity, TParameter> action)
         {
@@ -82,6 +128,12 @@ namespace XenoEngine
             return entityEvent;
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// operator -. overrides + operator to subtract handlers to events easily.
+        /// </summary>
+        /// <param name="entityEvent">the event you are remove to another event.</param>
+        /// <param name="action">the event handler</param>
+        /// <returns></returns>
         //-------------------------------------------------------------------------------
         public static EntityEvent<TParameter> operator -(EntityEvent<TParameter> entityEvent, Action<Entity, TParameter> action)
         {
@@ -89,12 +141,20 @@ namespace XenoEngine
             return entityEvent;
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// clears the event handler.
+        /// </summary>
         //-------------------------------------------------------------------------------
         public void Clear()
         {
             m_delegate = null;
         }
         //-------------------------------------------------------------------------------
+        /// <summary>
+        /// fires the event.
+        /// </summary>
+        /// <param name="sendingEntity">the entity the event is coming from.</param>
+        /// <param name="parameter">data to be sent with the event.</param>
         //-------------------------------------------------------------------------------
         public void FireEvent(Entity sendingEntity, TParameter parameter)
         {
