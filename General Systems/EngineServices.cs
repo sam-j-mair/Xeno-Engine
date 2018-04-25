57d728094501a82b978d71173619220b514f5f8d
Sam Mair <sam.j.mair@gmail.com>
2018-04-25 12:53:38 +1000

    updated xml comments for engine services.

--- General Systems/EngineServices.cs
+++ General Systems/EngineServices.cs
@@ -4,8 +4,21 @@ using System.Diagnostics;
 
 namespace XenoEngine.GeneralSystems
 {
+    /// <summary>
+    /// This is a singleton class that allows easy access to the services that the engine uses.
+    /// it also allow registration.
+    /// </summary>
     public static class EngineServices
     {
+        private static Dictionary<Type, ISystems> m_services;
+
+        //----------------------------------------------------------------------------------
+        /// <summary>
+        /// Get a registered service.
+        /// </summary>
+        /// <typeparam name="TISystemsType">the typeof the service to get that it is registered under.</typeparam>
+        /// <returns></returns>
+        //----------------------------------------------------------------------------------
         public static TISystemsType GetSystem<TISystemsType>() where TISystemsType : ISystems
         {
             ISystems iSystem;
@@ -19,8 +32,13 @@ namespace XenoEngine.GeneralSystems
                 return default(TISystemsType);
             }
         }
-        private static Dictionary<Type, ISystems> m_services;
-
+        //----------------------------------------------------------------------------------
+        /// <summary>
+        /// allows for registering a new system.
+        /// </summary>
+        /// <typeparam name="TISystemsType">the type of the service to be registered.</typeparam>
+        /// <param name="provider">the service provider.</param>
+        //----------------------------------------------------------------------------------
         public static void RegisterSystems<TISystemsType>(ISystems provider) where TISystemsType : ISystems
         {
             Type type = provider.GetType();
@@ -35,7 +53,11 @@ namespace XenoEngine.GeneralSystems
                 Debug.Fail("This service already exists!");
             }
         }
-
+        //----------------------------------------------------------------------------------
+        /// <summary>
+        /// Lazy Initialization.
+        /// </summary>
+        //----------------------------------------------------------------------------------
         private static void LazyInit()
         {
             if (m_services == null)
